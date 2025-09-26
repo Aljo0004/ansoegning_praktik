73408b6afcdbeaac2c624de3d1d125b697c0b263
Aljo0004 <aljo0004@stud.kea.dk>
2025-09-26 18:49:31 +0200

    changes

---
+++ js.js
@@ -0,0 +1,25 @@
+// menu //
+const burger = document.querySelector(".burger");
+const nav = document.querySelector("nav");
+const menu = document.querySelector(".menu");
+
+burger.addEventListener("click", burgerClick);
+function burgerClick() {
+  burger.classList.toggle("active");
+  nav.classList.toggle("active");
+}
+menu.addEventListener("click", menuClick);
+function menuClick() {
+  burger.classList.remove("active");
+  nav.classList.remove("active");
+}
+
+//kompetencer//
+const contents = document.querySelectorAll(".content");
+
+contents.forEach((indhold) => {
+  const carousel = indhold.querySelector(".img_carousel");
+  indhold.addEventListener("click", () => {
+    carousel.classList.toggle("active");
+  });
+});
